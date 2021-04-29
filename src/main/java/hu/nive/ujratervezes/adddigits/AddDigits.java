e2fb95f01ea2111dca2e23e26c7f162c588a90dd
Horszab <69106777+Horszab@users.noreply.github.com>
2021-04-29 10:16:31 +0200

    test done

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,28 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits(String input) {
+
+        if (input == null || input.equals("")) {
+            return -1;
+        }
+
+        int sum = 0;
+        char[] array = input.toCharArray();
+        
+        for (char c : array) {
+            String temp = Character.toString(c);
+
+            try {
+                int charValue = Integer.parseInt(temp);
+                sum += charValue;
+            } catch (Exception ignored) {}
+
+        }
+
+        return sum;
+
+    }
+
 }
