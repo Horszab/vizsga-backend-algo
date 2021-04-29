9b4c64a54c73ef77bcb09d8ca96b23e543ded39e
Horszab <69106777+Horszab@users.noreply.github.com>
2021-04-29 12:14:35 +0200

    formatted

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -5,19 +5,25 @@ public class AddDigits {
     public int addDigits(String input) {
 
         if (input == null || input.equals("")) {
+
             return -1;
+
         }
 
         int sum = 0;
         char[] array = input.toCharArray();
-        
+
         for (char c : array) {
+
             String temp = Character.toString(c);
 
             try {
+
                 int charValue = Integer.parseInt(temp);
                 sum += charValue;
-            } catch (Exception ignored) {}
+
+            } catch (Exception ignored) {
+            }
 
         }
 
